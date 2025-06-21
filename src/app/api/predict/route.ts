3fcc44f81e79e3765fb981929a634f15dc368649
Thejaswi S <thejaswi4uns@gmail.com>
2025-06-21 10:59:45 +0530

    Modified

--- src/app/api/predict/route.ts
+++ src/app/api/predict/route.ts
@@ -82,7 +82,6 @@ export async function POST(req: NextRequest) {
           prediction: age,
           image_url: publicUrl,
           user_id: session.user.id,
-          created_at: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
         },
       ]);
 
